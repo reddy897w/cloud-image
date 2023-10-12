a66a12960ee030d80173f892604727e1d025b3dd
appled0g-12 <147696617+reddy897w@users.noreply.github.com>
2023-10-12 13:19:44 +0530

    Update app.py

--- app.py
+++ app.py
@@ -130,4 +130,4 @@ if __name__ == '__main__':
     app.secret_key = 'super secret key'
     app.config['SESSION_TYPE'] = 'filesystem'
 
-    app.run(host='0.0.0.0', debug=True)
+    app.run(host='0.0.0.0', debug=False)
