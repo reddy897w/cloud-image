0a2fdfffb748f07e5908d6d99f0642aa8425faea
appled0g-12 <147696617+reddy897w@users.noreply.github.com>
2023-10-12 19:59:39 +0530

    updates

--- Dockerfile
+++ Dockerfile
@@ -1,10 +1,24 @@
-FROM python:3.11.4
+# Use the official Python image as the base image
+FROM python:3.8
 
+# Set the working directory inside the container
 WORKDIR /app
-COPY . /app
 
-RUN pip install --no-cache-dir -r requirements.txt
+# Copy the requirements file into the container
+COPY app/requirements.txt .
 
-EXPOSE 5000
+# Install the Python dependencies
+RUN pip install -r requirements.txt
 
-CMD ["python", "app.py"]
+# Copy the entire app directory into the container
+COPY app/ .
+
+# Expose the port on which your Flask app will run
+EXPOSE 8080
+
+# Set environment variables
+ENV GOOGLE_CLIENT_ID=your_google_client_id
+ENV SESSION_SECRET_KEY=your_session_secret_key
+
+# Run your Flask app
+CMD ["python", "your_flask_app.py"]
--- app/app.py
+++ app/app.py
@@ -1,35 +1,30 @@
 import os
-import pathlib
 from datetime import datetime
-from flask import Flask, render_template, request, redirect, url_for, flash, make_response, g, session, abort
-import requests
+from flask import Flask, render_template, request, redirect, url_for, session, abort
 from google.oauth2 import id_token
 from google_auth_oauthlib.flow import Flow
-from pip._vendor import cachecontrol
-import google.auth.transport.requests
 from google.cloud import storage, datastore
 
 app = Flask(__name__)
 app.secret_key = os.environ.get('SESSION_SECRET_KEY', 'image-manager-app')
 
-os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = './service-account-key.json'
-os.environ["OAUTHLIB_INSECURE_TRANSPORT"] = "1"
+# Set your Google OAuth2 client ID
+GOOGLE_CLIENT_ID = os.environ.get('GOOGLE_CLIENT_ID', '110981627320-n8tfb1l2lpodpojmgqkfg45q84rs630n.apps.googleusercontent.com')
 
+# Path to your client secret file
+CLIENT_SECRETS_FILE = './app/client_secret.json'
+
+# Set up OAuth2 flow
+flow = Flow.from_client_secrets_file(
+    client_secrets_file=CLIENT_SECRETS_FILE,
+    scopes=["https://www.googleapis.com/auth/userinfo.profile", "https://www.googleapis.com/auth/userinfo.email", "openid"],
+    redirect_uri="http://your-app-url.com/callback"
+)
+
+# Configure Google Cloud Storage and Datastore clients using the service account key
+os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = './app/service-account-key.json'
 storage_client = storage.Client()
 datastore_client = datastore.Client()
-
-BUCKET_NAME = "cloud-image-app"
-
-# Store sensitive information in environment variables
-GOOGLE_CLIENT_ID = os.environ.get('GOOGLE_CLIENT_ID', "110981627320-n8tfb1l2lpodpojmgqkfg45q84rs630n.apps.googleusercontent.com")
-
-client_secrets_file = os.path.join(pathlib.Path(__file__).parent, "client_secret.json")
-
-flow = Flow.from_client_secrets_file(
-    client_secrets_file=client_secrets_file,
-    scopes=["https://www.googleapis.com/auth/userinfo.profile", "https://www.googleapis.com/auth/userinfo.email", "openid"],
-    redirect_uri="http://127.0.0.1:5000/callback"
-)
 
 # Helper function to check if a user is logged in
 def login_is_required(function):
@@ -55,13 +50,9 @@ def callback():
         abort(500)  # State does not match!
 
     credentials = flow.credentials
-    request_session = requests.session()
-    cached_session = cachecontrol.CacheControl(request_session)
-    token_request = google.auth.transport.requests.Request(session=cached_session)
-
     id_info = id_token.verify_oauth2_token(
-        id_token=credentials._id_token,
-        request=token_request,
+        id_token=credentials.id_token,
+        request=request,
         audience=GOOGLE_CLIENT_ID
     )
 
@@ -79,22 +70,22 @@ def logout():
 @app.route('/index', methods=['GET', 'POST'])
 @login_is_required
 def index():
-    email = g.get('email', '')
+    email = session.get('email', '')
 
     if request.method == 'POST':
         image = request.files['imageInput']
         filename_parts = os.path.splitext(image.filename)
         unique_filename = f"{filename_parts[0]}_{datetime.now().strftime('%Y%m%d%H%M%S%f')}{filename_parts[1]}"
 
-        # Upload image to GCP Storage
-        bucket = storage_client.bucket(BUCKET_NAME)
+        # Upload image to Google Cloud Storage
+        bucket = storage_client.bucket('your-bucket-name')
         blob = bucket.blob(unique_filename)
         blob.upload_from_string(
             image.read(),
             content_type=image.content_type
         )
 
-        # Save image metadata to Datastore
+        # Save image metadata to Google Cloud Datastore
         entity = datastore.Entity(
             key=datastore_client.key('Image', unique_filename))
         entity.update({
@@ -118,20 +109,20 @@ def index():
 # View image route
 @app.route('/view/<filename>')
 def view_image(filename):
-    image_url = f"https://storage.googleapis.com/cloud-image-app/{filename}"
+    image_url = f"https://storage.googleapis.com/your-bucket-name/{filename}"
     return render_template('view.html', image_url=image_url, filename=filename)
 
 # Uploaded file route
 @app.route('/uploads/<filename>')
 def uploaded_file(filename):
-    image_url = f"https://storage.googleapis.com/cloud-image-app/{filename}"
+    image_url = f"https://storage.googleapis.com/your-bucket-name/{filename}"
     return redirect(image_url)
 
 # Delete image route
 @app.route('/delete/<filename>')
 def delete_image(filename):
-    # Delete image from GCP Storage
-    bucket = storage_client.bucket(BUCKET_NAME)
+    # Delete image from Google Cloud Storage
+    bucket = storage_client.bucket('your-bucket-name')
     blob = bucket.blob(filename)
     blob.delete()
 
@@ -141,6 +132,5 @@ def delete_image(filename):
 
     return redirect(url_for('index'))
 
-if __name__ == '__main__':
-    app.config['SESSION_TYPE'] = 'filesystem'
-    app.run(host='0.0.0.0', debug=True)
+if __name__ == '__main':
+    app.run(host='0.0.0.0', port=8080)
